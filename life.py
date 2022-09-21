6713a57dc29e5203ca24e746577dc00cbb03b772
SlowMatrix <113237772+SlowMatrix@users.noreply.github.com>
2022-09-21 15:13:34 +0300

    Add files via upload

--- life.py
+++ life.py
@@ -2,7 +2,7 @@ def life(w, q, im, v, mur):
     mu = sum(mur)
     return (mu == 3 and im == 0) or (im == 1 and (mu == 2 or mu == 3))
 
-def main():
+def main_consol():
     import time
     from class_matrix import Matrix
     a = True
@@ -25,6 +25,31 @@ def main():
         if s == False:
             main.pattern("random", [0, 1])  
             
+def main_pygame():
+    import time
+    from show_in_pygame import show_in_pygame
+    from class_matrix import Matrix
+    
+    a = True
+    main = Matrix(67, 33)
+    main.pattern("random", [0, 1])  
+    
+    def a(x):
+        if x > 0:
+            return "▀"
+        return "□"
+    
+    time.sleep(1)
+    s = True
+    while s:
+        for i in range(100):
+            main.scan(life, mura=True)
+            show_in_pygame.show_nums(main, immn=255, time=0.1)
+            time.sleep(0.01)
+        s = bool(input())
+        if s == False:
+            main.pattern("random", [0, 1])  
+
 if __name__ == "__main__":
-    main()
+    main_pygame()
 
--- show_in_pygame.py
+++ show_in_pygame.py
@@ -1,64 +1,55 @@
+class Matrix_display:
+    def __init__(self, display, matrix, update_function=None):
+        self.display = display
+        self.matrix = matrix
+        
+    def update_display(self):
+        if self.func != None:
+            self.matrix.scan(self.func, murra = True)
+    
+    def cluse(self):
+        pg.quit()
+
 class show_in_pygame: # использую класс только для красивого использования
     def show_nums(m, sizecl=10, immn=1, Display=None, time=3):
         import pygame as pg
         import time as t
         
-        def f(x, y, im, inF):
+        def f(x, y, im, display):
             colour = [255 if (s := im*immn) > 255 else s]*3
             rect = (x*sizecl, y*sizecl, sizecl, sizecl)
             pg.draw.rect(display, colour, rect)
         
-        if Display == None:
-            display = pg.display.set_mode((0, 0))
-        else:
-            display = Display
+        show_in_pygame.show_by_func(m, f, Display, time)
         
-        m = m.copy()
-        m.scan_no_ret(f)
-        
-        pg.display.update()
-        t.sleep(time)
-        if Display == None:
-            pg.quit()
-            
     def show_nums_3D(m, sizecl=10, immn=1, Display=None, time=3, Cof3D=1.25):
+        import pygame as pg
+        def f(x, y, im, display):
+            colour = [y**1.1]+[im]+[255 if (s := im*immn) > 255 else s]
+            colour = [i if i <= 255 and i > 0 else 255 for i in colour]
+            rect = (x*sizecl, y*sizecl-im**Cof3D, sizecl, sizecl)
+            pg.draw.rect(display, colour, rect)
+        
+        show_in_pygame.show_by_func(m, f, Display, time)
+        
+    def show_by_func(m, f, Display=None, time=3):
         import pygame as pg
         import time as t
            
-        def f(x, y, im, inF):
-            colour = [y**1.1]+[255 if (s := im*immn) > 255 else s]*2 # !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! y может превысить 255
-            rect = (x*sizecl, y*sizecl-im**Cof3D, sizecl, sizecl)
-            pg.draw.rect(display, colour, rect)
-            
         if Display == None:
             display = pg.display.set_mode((0, 0))
         else:
             display = Display
             
         m = m.copy()
-        m.scan_no_ret(f)
+        m.scan_no_ret(f, inFunk=display)
             
-        pg.display.update()
-        t.sleep(time)
+        
         if Display == None:
-            pg.quit()  
-
-        def show_by_func(m, f, sizecl=10, immn=1, Display=None, time=3):
-            import pygame as pg
-            import time as t
-            
-            if Display == None:
-                display = pg.display.set_mode((0, 0))
-            else:
-                display = Display
-            
-            m = m.copy()
-            m.scan_no_ret(f, inFunk=display)
-            
             pg.display.update()
             t.sleep(time)
-            if Display == None:
-                pg.quit()
+            pg.quit()
+            
 
         
 
@@ -66,5 +57,6 @@ class show_in_pygame: # использую класс только для кра
 if __name__ == "__main__":
     from generator import *
     from class_matrix import Matrix
-    m = perlin_nose(Matrix(100, 100), 4, (0, 255))
-    show_in_pygame.show_nums(m.show(), time=1)
+    m = perlin_nose(Matrix(100, 100), 9, (0, 255))
+    show_in_pygame.show_nums(m, time=1)
+    show_in_pygame.show_nums_3D(m, time=1, Cof3D=1.001)
